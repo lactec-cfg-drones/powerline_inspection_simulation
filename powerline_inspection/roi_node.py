a2eb9954295bf67703590f7e2ecc0b85c6cdfe71
LeticiaRP <leticiarp2000@hotmail.com>
2026-03-13 15:38:16 -0300

    chore: enhance ROI node with data saving functionality and improved depth filtering

--- powerline_inspection/roi_node.py
+++ powerline_inspection/roi_node.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3 
-import numpy as np 
+import os
 import rclpy 
+import numpy as np 
 import sensor_msgs_py.point_cloud2 as pc2
 
 from rclpy.node import Node
+from std_msgs.msg import Empty
 from sensor_msgs.msg import PointCloud2
 from vision_msgs.msg import Detection2DArray
 
@@ -15,18 +17,16 @@ from scipy.spatial.transform import Rotation as R
 
 
 
+
 class RoiNode(Node): 
 
     def __init__(self):
         super().__init__('roi_node')
-
     
         self.last_yolo_bbox = None
         
-
         # camera frame 
-        self.target_frame = 'camera_front_link' # Frame da câmera
-        
+        self.target_frame = 'camera_front_link' 
 
         # camera config (from the urdf model)
         self.fx = 701.93225
@@ -36,20 +36,36 @@ class RoiNode(Node):
         self.width = 1600
         self.height = 1200
 
-        
         # tf configs 
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
 
-
         # subscribers 
         self.lidar_sub = self.create_subscription(PointCloud2, '/front_laser/point_cloud', self.lidar_callback, 10)
         self.yolo_sub = self.create_subscription(Detection2DArray, '/yolo/detections', self.yolo_callback, 10)
+        self.save_cmd_sub = self.create_subscription(Empty, '/cmd/save_roi', self.save_cmd_callback, 10)
         
         # publishers
         self.roi_pub = self.create_publisher(PointCloud2, '/perception/point_cloud', 10) 
 
-        self.lastest_detections = []
+        # Config para salvar dados
+        self.save_requested = False
+        self.data_dir = 'dataset_pointnet'
+        self.save_count = 0
+        
+        if not os.path.exists(self.data_dir):
+            os.makedirs(self.data_dir)
+
+
+
+
+
+
+
+    def save_cmd_callback(self, msg): 
+        self.save_requested = True
+        self.get_logger().info('✅ Comando de salvamento recebido! Capturando próximo frame...')
+
 
 
 
@@ -66,16 +82,15 @@ class RoiNode(Node):
 
 
 
-    # transform lidar frame to camera frame 
     def apply_transform(self, points, transform):
+        # Garantir que não há NaNs nos pontos antes da conta
+        points = points[np.all(np.isfinite(points), axis=1)]
         
         q = transform.transform.rotation
         rot_matrix = R.from_quat([q.x, q.y, q.z, q.w]).as_matrix()
-        
         t = transform.transform.translation
         translation = np.array([t.x, t.y, t.z])
         
-        # P_cam = R * P_lidar + T
         return (points @ rot_matrix.T) + translation
 
 
@@ -87,38 +102,34 @@ class RoiNode(Node):
         if self.last_yolo_bbox is None:
             return
 
-
-        # get tf data 
         try:
             source_frame = msg.header.frame_id
-            trans = self.tf_buffer.lookup_transform(self.target_frame, source_frame, rclpy.time.Time())
+           
+            trans = self.tf_buffer.lookup_transform(
+                self.target_frame, 
+                source_frame, 
+                rclpy.time.Time()) 
         except Exception:
             return
         
-        
-        # convert point cloud to numpy array and discart nan values 
+        # conversion and remove nans 
         gen = pc2.read_points(msg, field_names=('x', 'y', 'z'), skip_nans=True)
         pts_lidar = np.array([[p[0], p[1], p[2]] for p in gen], dtype=np.float32)
-        pts_lidar = pts_lidar[np.all(np.isfinite(pts_lidar), axis=1)]
         if pts_lidar.size == 0: return
 
 
-        # frames transforms 
+        # transform to he camera frame
         pts_cam = self.apply_transform(pts_lidar, trans)
 
-       
-        # ajust optical frame  
-        # at gazebo, x=ahead, for the ros projection we need to put z ahead with a 90 degree rotation
-        # P_optical_x = -P_cam_y
-        # P_optical_y = -P_cam_z
-        # P_optical_z =  P_cam_x
+
+        # ajust optical frame 
         x_opt = -pts_cam[:, 1]
         y_opt = -pts_cam[:, 2]
         z_opt =  pts_cam[:, 0]
 
 
-        # depth filter to discart the readings close to the drone
-        valid_mask = z_opt > 1.5 
+        # descart the samples cllose to the drone 
+        valid_mask = z_opt > 2.0 
         x_opt, y_opt, z_opt = x_opt[valid_mask], y_opt[valid_mask], z_opt[valid_mask]
         pts_final = pts_cam[valid_mask]
 
@@ -126,37 +137,72 @@ class RoiNode(Node):
         if z_opt.size == 0: return
 
 
-        # image projection 
+        # image projection
         u = (self.fx * x_opt / z_opt) + self.cx
         v = (self.fy * y_opt / z_opt) + self.cy
 
 
-        # get region of interest from yolo
+        # get the roi from yolo (more a margin of 10%)
         b = self.last_yolo_bbox
-        u_min, u_max = b.center.position.x - b.size_x/2, b.center.position.x + b.size_x/2
-        v_min, v_max = b.center.position.y - b.size_y/2, b.center.position.y + b.size_y/2
+        pad = 1.10 
+        u_min, u_max = b.center.position.x - (b.size_x/2)*pad, b.center.position.x + (b.size_x/2)*pad
+        v_min, v_max = b.center.position.y - (b.size_y/2)*pad, b.center.position.y + (b.size_y/2)*pad
 
 
-        mask_u = (u >= u_min) & (u <= u_max)
-        mask_v = (v >= v_min) & (v <= v_max)
+        # depth of the window
+        spatial_mask = (u >= u_min) & (u <= u_max) & (v >= v_min) & (v <= v_max)
+
+
+        if not np.any(spatial_mask):
+            return
+
+
+        z_tower = z_opt[spatial_mask]
+        median_depth = np.median(z_tower)
         
 
-        roi_mask = mask_u & mask_v & (z_opt < 50.0)
+        thickness = 8.0 
+        depth_mask = (z_opt >= (median_depth - thickness/2)) & (z_opt <= (median_depth + thickness/2))
+        
+        roi_mask = spatial_mask & depth_mask
         roi_points = pts_final[roi_mask]
 
 
-        print(f"U min/max: {u.min():.1f}/{u.max():.1f} | V min/max: {v.min():.1f}/{v.max():.1f}")
-
-
+        # publish and save data 
         if len(roi_points) > 0:
-            # publish data 
             roi_msg = pc2.create_cloud_xyz32(msg.header, roi_points)
             roi_msg.header.frame_id = self.target_frame
             self.roi_pub.publish(roi_msg)
-            self.get_logger().info(f"ALVO LOCALIZADO: {len(roi_points)} pontos na torre.")
-        
+            
+
+            if len(roi_points) < 50:
+                self.get_logger().warn(f"Poucos pontos capturados ({len(roi_points)}). Verifique o alinhamento.")
+            else:
+                self.get_logger().info(f"ALVO LOCALIZADO: {len(roi_points)} pontos.")
+
+
+            if self.save_requested: 
+                # data normalization
+                centroid = np.mean(roi_points, axis=0)
+                pts_normalized = roi_points - centroid
+
+
+                # normalize scale 
+                max_dist = np.max(np.sqrt(np.sum(pts_normalized**2, axis=1)))
+                if max_dist > 0:
+                    pts_normalized = pts_normalized / max_dist
+
+                timestamp = self.get_clock().now().nanoseconds
+                filename = os.path.join(self.data_dir, f"tower_{timestamp}.npy")
+                np.save(filename, pts_normalized)
+                
+
+                self.save_count += 1
+                self.get_logger().info(f'💾 Nuvem #{self.save_count} salva e normalizada! Arquivo: {filename}')
+                self.save_requested = False
         else:
-            self.get_logger().warn(f"ROI empty. U_m: {np.mean(u):.1f}, V_m: {np.mean(v):.1f}")
+            # Feedback de debug if the ROI is empty 
+            self.get_logger().warn("ROI is empty.")
 
 
 
