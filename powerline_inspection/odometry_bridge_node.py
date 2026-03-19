81c3ffefb20a4d982f9b7d427666d38af8ba671e
LeticiaRP <leticiarp2000@hotmail.com>
2026-03-19 10:42:58 -0300

    chore: add GPS subscription to DroneTFBroadcaster for origin frame definition

--- powerline_inspection/odometry_bridge_node.py
+++ powerline_inspection/odometry_bridge_node.py
@@ -4,7 +4,7 @@ from rclpy.node import Node
 from tf2_ros import TransformBroadcaster
 from geometry_msgs.msg import TransformStamped
 from nav_msgs.msg import Odometry
-from sensor_msgs.msg import Imu
+from sensor_msgs.msg import Imu, NavSatFix
 import transforms3d as tf3d 
 
 class DroneTFBroadcaster(Node):
@@ -12,27 +12,27 @@ class DroneTFBroadcaster(Node):
     def __init__(self):
         super().__init__('drone_tf_broadcaster')
 
-        # Initializing the transform broadcaster
         self.tf_broadcaster = TransformBroadcaster(self)
 
-        # Subscribers for bridged topics
-        self.odom_sub = self.create_subscription(
-            Odometry,
-            '/odom',
-            self.handle_odom,
-            10)
-        
-        self.imu_sub = self.create_subscription(
-            Imu,
-            '/imu',
-            self.handle_imu,
-            10)
+        # subscribers
+        self.odom_sub = self.create_subscription(Odometry, '/odom', self.handle_odom, 10)
+        self.imu_sub = self.create_subscription(Imu, '/imu', self.handle_imu, 10)
+        self.gps_sub = self.create_subscription(NavSatFix, '/navsat', self.handle_gps, 10)
+
+        self.origin_lat = None
+        self.origin_lon = None
 
         self.get_logger().info('Drone TF Broadcaster Node Started Successfully')
+
+
+
 
     def handle_imu(self, msg):
         # Optional: You can extract heading (yaw) here if your odom lacks orientation
         pass
+
+
+
 
     def handle_odom(self, msg):
         now = self.get_clock().now().to_msg() #
@@ -45,10 +45,22 @@ class DroneTFBroadcaster(Node):
         
         t_odom_footprint.transform.translation.x = msg.pose.pose.position.x
         t_odom_footprint.transform.translation.y = msg.pose.pose.position.y
-        t_odom_footprint.transform.translation.z = 0.0
+        t_odom_footprint.transform.translation.z = msg.pose.pose.position.z 
         t_odom_footprint.transform.rotation = msg.pose.pose.orientation
         
-        self.tf_broadcaster.sendTransform(t_odom_footprint) #
+        self.tf_broadcaster.sendTransform(t_odom_footprint) 
+    
+
+
+    def handle_gps(self, msg): 
+        # define the first data as the origin of the frame world
+        if self.origin_lat is None: 
+            self.origin_lat = msg.latitude
+            self.origin_lon = msg.longitude
+
+            self.get_logger().info(f'Origin of the frame world defined at: {self.origin_lat}, {self.origin_lon}')
+
+
 
 def main(args=None):
     rclpy.init(args=args)
