3570c14d65814d7bab4f2f1acba31ea127c9968c
LeticiaRP <leticiarp2000@hotmail.com>
2026-03-19 10:42:43 -0300

    chore: add QoS configuration for laser scans, point clouds, and images in drone bridge launch file

--- launch/drone_bridge.launch.py
+++ launch/drone_bridge.launch.py
@@ -36,6 +36,58 @@ def generate_launch_description():
             'config_file': config_file_path,
             'qos_overrides./tf_static.publisher.durability': 'transient_local',
             'qos_overrides./air_pressure.publisher.reliability': 'best_effort',
+
+
+            # laser scan configuration 
+            'qos_overrides./front_laser/scan.reliability': 'best_effort',
+            'qos_overrides./front_laser/scan.durability': 'volatile',
+            'qos_overrides./front_laser/scan.history': 'keep_last',
+            'qos_overrides./front_laser/scan.depth': '5',
+            
+
+            # point cloud configuration
+            'qos_overrides./front_laser/point_cloud.reliability': 'best_effort',
+            'qos_overrides./front_laser/point_cloud.durability': 'volatile',
+            'qos_overrides./front_laser/scan.history': 'keep_last',
+            'qos_overrides./front_laser/point_cloud.depth': '5',
+
+            'qos_overrides./rs_down/point_cloud.reliability': 'best_effort',
+            'qos_overrides./rs_down/point_cloud.durability': 'volatile',
+            'qos_overrides./rs_down/point_cloud.history': 'keep_last',
+            'qos_overrides./rs_down/point_cloud.depth': '5',
+
+            'qos_overrides./rs_up/point_cloud.reliability': 'best_effort',
+            'qos_overrides./rs_up/point_cloud.durability': 'volatile',
+            'qos_overrides./rs_up/point_cloud.history': 'keep_last',
+            'qos_overrides./rs_up/point_cloud.depth': '5',
+
+
+            # image configuration 
+            'qos_overrides./camera_front/image.reliability': 'best_effort',
+            'qos_overrides./camera_front/image.durability': 'volatile',
+            'qos_overrides./camera_front/image.history': 'keep_last',
+            'qos_overrides./camera_front/image.depth': '5',
+
+            'qos_overrides./rs_down/depth_image.reliability': 'best_effort',
+            'qos_overrides./rs_down/depth_image.durability': 'volatile',
+            'qos_overrides./rs_down/depth_image.history': 'keep_last',
+            'qos_overrides./rs_down/depth_image.depth': '5',
+
+            'qos_overrides./rs_down/image.reliability': 'best_effort',
+            'qos_overrides./rs_down/image.durability': 'volatile',
+            'qos_overrides./rs_down/image.history': 'keep_last',
+            'qos_overrides./rs_down/image.depth': '5',
+
+            'qos_overrides./rs_up/depth_image.reliability': 'best_effort',
+            'qos_overrides./rs_up/depth_image.durability': 'volatile',
+            'qos_overrides./rs_up/depth_image.history': 'keep_last',
+            'qos_overrides./rs_up/depth_image.depth': '5',
+
+            'qos_overrides./rs_up/image.reliability': 'best_effort',
+            'qos_overrides./rs_up/image.durability': 'volatile',
+            'qos_overrides./rs_up/image.history': 'keep_last',
+            'qos_overrides./rs_up/image.depth': '5',
+
             'use_sim_time': use_sim_time,
         }],
         output='screen'
